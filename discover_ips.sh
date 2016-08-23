7a333f22871001e5b77f2e88444c62c6254ecfa9
Epzylon <lord.epzyon@gmail.com>
2016-08-23 11:19:20 -0300

    discover ips on local networks script

---
+++ discover_ips.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+IP=$(which ip)
+FPING=$(which fping)
+NET_LIST=$($IP -4 -o addr ls | awk '!/127.0.0.1/ { print $4 }')
+FPING_COMMAND="$FPING -ag"
+
+#NULL_OUTPUT="2>/dev/null"
+if [[ $1 == "-v" ]];
+then
+	VER="yes"
+else
+	VER="no"
+fi
+echo $NULL_OUTPUT
+
+#Lets search for conneced interfaces
+for net in $NET_LIST;
+do
+	if [[ $VER -eq "yes" ]];
+	then
+			echo "IPs on Network $net:"
+			$FPING_COMMAND $net
+	else
+			$FPING_COMMAND $net 2>/dev/null
+done
